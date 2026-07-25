f908810b8591bc3e908a0e217afaa4ac4282e596
Akula Akshay <akshayakula13@gmail.com>
2026-07-25 22:26:54 +0530

    some related info

---
+++ redis.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m" #or 0m
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #full path - /var/log/shell-script/16-logs.log
+
+START_TIME=$(date +%s)
+
+mkdir -p $LOGS_FOLDER
+echo "script started executed at : $(date)" | tee -a $LOG_FILE #tee is used because by echo statemnet it prints in the terminal but dosen't
+# store in the logs, in order to print in the terminal and store in the logs we use tee command.
+
+if [ $USERID -ne 0 ]; then 
+    echo "Error: please run the command with the root privilages"
+    exit 1 # when we have a probability of failure we need to provide exit code as non-zero
+fi
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+      echo -e "ERROR $2 is $R failed $N" | tee -a $LOG_FILE
+      exit 1
+    else
+      echo -e "$2 is $G success $N" | tee -a $LOG_FILE
+      #exit 0 -- if we want we can prvide but by default it takes 0 only
+    fi
+
+}
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disable default redis version"
+
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enable redis 7 version"
+
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Install redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing Remote connections to Redis"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enable redis"
+
+systemctl start redis &>>$LOG_FILE 
+VALIDATE $? "start redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=(($END_TIME - $START_TIME))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
