db5affe0fe4844b490cc4fc7aba8f80530f2826b
Akula Akshay <akshayakula13@gmail.com>
2026-07-26 12:23:01 +0530

    some info about project

--- redis.sh
+++ redis.sh
@@ -53,5 +53,5 @@ systemctl start redis &>>$LOG_FILE
 VALIDATE $? "start redis"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=(( $END_TIME - $START_TIME ))
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
